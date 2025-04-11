089c603a21b3293991066f7093ebc2f616d503d7
Morgan Wigmanich <okize123@gmail.com>
2025-04-11 08:00:00 -0400

    ci: add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  singleAttributePerLine: true,
+};
+
+export default config;
