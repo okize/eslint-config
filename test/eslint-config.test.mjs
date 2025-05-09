308da25f1d4283cc3b762c4ad8264311bfacee4c
Morgan Wigmanich <okize123@gmail.com>
2025-05-09 08:00:00 -0400

    test: update eslint-config.test.js to handle circular references

--- test/eslint-config.test.mjs
+++ test/eslint-config.test.mjs
@@ -8,11 +8,30 @@ const configsDir = join(__dirname, '../src/eslint/configs');
 const snapshotsDir = join(__dirname, '__snapshots__');
 const configs = readdirSync(configsDir).map((file) => [basename(file, '.mjs'), file]);
 
+// helper to safely serialize JavaScript objects that contain circular references
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (_key, value) => {
+    // skip non-object values
+    if (typeof value !== 'object' || value === null) {
+      return value;
+    }
+
+    if (seen.has(value)) {
+      return '[Circular Reference]';
+    }
+
+    seen.add(value);
+
+    return value;
+  };
+};
+
 describe('ESLint Configs', () => {
   test.each(configs)('%s config matches snapshot', async (_configName, file) => {
     const configPath = join(configsDir, file);
     const config = await import(configPath);
-    const serializedConfig = JSON.stringify(config.default, null, 2);
+    const serializedConfig = JSON.stringify(config.default, getCircularReplacer(), 2);
     const snapshotPath = join(snapshotsDir, `${basename(file)}.snap`);
 
     await expect(serializedConfig).toMatchFileSnapshot(snapshotPath);
